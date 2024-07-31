02ef1df3e0f686cdc81833507cd4f9a5903eeb20
Kerr Hamilton <kerr9928@gmail.com>
2024-07-31 20:11:39 +0000

    Add function spin slot machine to work space with comment above

--- run.py
+++ run.py
@@ -44,3 +44,23 @@ def check_winnings(columns, lines, bet, values):
 
 
     return winnings, winning_lines
+
+# Function for generating random rows/columns of symbols
+def spin_slot_machine(rows, cols, symbols):
+    all_symbols = []
+    for symbol,symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
